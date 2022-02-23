a1708e5de1afd9eb930d380da68683f99e0d2d59
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-23 17:11:29 +0100

    Added initialization function to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+require("dotenv").config();
 const chalk = require("chalk");
-const connectDataBase = require("./database");
 const debug = require("debug")("series");
-const initializeServer = require("./server/initializeServer");
 const express = require("express");
+const connectDataBase = require("./database");
+const initializeServer = require("./server/initializeServer");
 
 const port = process.env.PORT || 4000;
 const dbUrl = process.env.MONGO_CONNECT;
-const app = express()(async () => {
+const app = express();
+
+(async () => {
   try {
     await connectDataBase(dbUrl);
     await initializeServer(port, app);
