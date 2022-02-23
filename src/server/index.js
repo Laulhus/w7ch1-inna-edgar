31e5f238e494ccb8f80e62b4c83f74dcf2a2eac5
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-23 19:30:36 +0100

    Created usersControllers.test.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const usersRouter = require("./routers/usersRouter");
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.json());
+
+app.use("/users", usersRouter);
 
 module.exports = app;
