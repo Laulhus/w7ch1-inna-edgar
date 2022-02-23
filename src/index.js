cf77111d81a98a3a6411faf559b23dc326e535d3
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-23 17:06:23 +0100

    Added nodemon and scripts to package.json

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const chalk = require("chalk");
+const connectDataBase = require("./database");
 const debug = require("debug")("series");
 const initializeServer = require("./server/initializeServer");
+const express = require("express");
 
 const port = process.env.PORT || 4000;
 const dbUrl = process.env.MONGO_CONNECT;
-
-(async () => {
+const app = express()(async () => {
   try {
+    await connectDataBase(dbUrl);
     await initializeServer(port, app);
     debug(chalk.greenBright.bgMagenta("Initialization succesful!"));
   } catch (error) {
