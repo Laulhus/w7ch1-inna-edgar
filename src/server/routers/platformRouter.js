610c5e2c7fe1300b9652f1103f104b70fb3a29f3
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-23 20:14:42 +0100

    Tested updatePlatform

--- src/server/routers/platformRouter.js
+++ src/server/routers/platformRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   createPlatform,
   getPlatforms,
+  updatePlatform,
 } = require("../controllers/platformControllers");
 
 const platformRouter = express.Router();
