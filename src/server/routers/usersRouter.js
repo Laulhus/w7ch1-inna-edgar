0ec3fc26651c805e64cdc53f4370706d780a74f4
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-24 22:44:16 +0100

    Created /register/ endpoint in usersRouter.js

--- src/server/routers/usersRouter.js
+++ src/server/routers/usersRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { userLogin } = require("../controllers/usersControllers");
+const { userLogin, userRegister } = require("../controllers/usersControllers");
 
 const router = express.Router();
 
 router.post("/login", userLogin);
+router.post("/register", userRegister);
 
 module.exports = router;
