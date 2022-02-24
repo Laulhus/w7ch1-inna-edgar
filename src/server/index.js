77d6cc15cdc0bb3c386c54fa20b5e80c3bb77cc3
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-24 18:01:16 +0100

    Created auth.js

--- src/server/index.js
+++ src/server/index.js
@@ -7,20 +7,16 @@ const usersRouter = require("./routers/usersRouter");
 const { notFoundError, generalError } = require("./middlewares/errors");
 const platformRouter = require("./routers/platformRouter");
 
-
 const app = express();
 
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.json());
 
-
 app.use("/users", usersRouter);
-
 app.use("/platforms", platformRouter);
 
 app.use(notFoundError);
 app.use(generalError);
-
 
 module.exports = app;
